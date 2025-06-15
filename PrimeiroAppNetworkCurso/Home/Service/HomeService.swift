8382866d84be7d33d7032b3268ae82ae99100abe
wevertonmonteiroc <55930553+wevertonmonteiroc@users.noreply.github.com>
2025-06-15 20:08:00 -0300

    Finalizei a Request.

--- PrimeiroAppNetworkCurso/Home/Service/HomeService.swift
+++ PrimeiroAppNetworkCurso/Home/Service/HomeService.swift
@@ -7,6 +7,68 @@
 
 import UIKit
 
-class HomeService: NSObject {
+import UIKit
 
+enum NetworkError: Error {
+    case invalidURL(url: String)
+    case noData
+    case invalidResponse
+    case decodingError(Error)
+    case networkFailure(Error)
+}
+
+extension NetworkError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .invalidURL(let url):
+            return "URL inválida -> \(url)"
+        case .noData:
+            return "Nenhum dado recebido da api"
+        case .invalidResponse:
+            return "Resposta inválida da api"
+        case .decodingError(let error):
+            return "Erro ao decodificar os dados: \(error.localizedDescription)"
+        case .networkFailure(let error):
+            return "Erro na rede: \(error.localizedDescription)"
+        }
+    }
+}
+
+class HomeService: NSObject {
+    
+    public func fetchRequest(completion: @escaping (Result<PersonList,NetworkError>) -> Void) {
+        let urlString: String = "https://run.mocky.io/v3/1328d142-9a04-44a2-861f-510b44b8ba92"
+        
+        guard let url: URL = URL(string: urlString) else {
+            completion(.failure(.invalidURL(url: urlString)))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error ) in
+            if let error {
+                completion(.failure(.networkFailure(error)))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            do {
+                let personList: PersonList = try JSONDecoder().decode(PersonList.self, from: data)
+                print("SUCESS -> \(#function)")
+                completion(.success(personList))
+            } catch {
+                print("ERROR -> \(#function)")
+                completion(.failure(.decodingError(error)))
+            }
+        }
+        task.resume()
+    }
 }
