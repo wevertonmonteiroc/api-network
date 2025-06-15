69260e716d359b6e69095f550f34b8d3de813490
wevertonmonteiroc <55930553+wevertonmonteiroc@users.noreply.github.com>
2025-06-15 17:18:53 -0300

    Iniciei o Service do Projeto.

---
+++ PrimeiroAppNetworkCurso/Home/Service/HomeService.swift
@@ -0,0 +1,12 @@
+//
+//  HomeService.swift
+//  PrimeiroAppNetworkCurso
+//
+//  Created by Weverton on 14/06/25.
+//
+
+import UIKit
+
+class HomeService: NSObject {
+
+}
---
+++ PrimeiroAppNetworkCurso/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewModel.swift
+//  PrimeiroAppNetworkCurso
+//
+//  Created by Weverton on 14/06/25.
+//
+
+import UIKit
+
+class HomeViewModel: NSObject {
+
+    public func fetchRequest() {
+        
+    }
+}
